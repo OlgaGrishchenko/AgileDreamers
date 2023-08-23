cafa21dc612c6e88ca4317f0c39a29100be07cb7
Cherkasant <antnchrks@gmail.com>
2023-08-23 22:34:51 +0300

    added SEO fixes

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,15 @@ function App() {
         <meta property="og:type" content="website"/>
         <meta property="og:title" content="AGILE DREAMERS"/>
         <meta property="og:description" content="B2B matching platform"/>
-        <meta property="og:image" content="../public/logo1200x630.png"/>
+        <meta property="og:image" content="https://agiledreamers.com/logo1200x630.png"/>
+        <meta property="og:image:width" content="1200"/>
+        <meta property="og:image:height" content="630"/>
         <meta name="twitter:card" content="summary_large_image"/>
         <meta property="twitter:domain" content="agiledreamers.com"/>
         <meta property="twitter:url" content="https://agiledreamers.com/"/>
         <meta name="twitter:title" content="AGILE DREAMERS"/>
         <meta name="twitter:description" content="B2B matching platform"/>
-        <meta name="twitter:image" content="../public/logo1200x630.png"/>
+        <meta name="twitter:image" content="https://agiledreamers.com/logo1200x630.png"/>
       </Helmet>
       <Router />
     </div>
